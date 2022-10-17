fa44925f0d4464561b6a1e4f5d0095a35d12333b
chuyu <1575057401@qq.com>
2022-10-17 19:35:48 +0800

    完成权限拦截功能

--- src/permission.js
+++ src/permission.js
@@ -1,64 +1,32 @@
-import router from './router'
-import store from './store'
-import { Message } from 'element-ui'
-import NProgress from 'nprogress' // progress bar
-import 'nprogress/nprogress.css' // progress bar style
-import { getToken } from '@/utils/auth' // get token from cookie
-import getPageTitle from '@/utils/get-page-title'
-
-NProgress.configure({ showSpinner: false }) // NProgress Configuration
-
-const whiteList = ['/login'] // no redirect whitelist
-
-router.beforeEach(async(to, from, next) => {
-  // start progress bar
-  NProgress.start()
-
-  // set page title
-  document.title = getPageTitle(to.meta.title)
-
-  // determine whether the user has logged in
-  const hasToken = getToken()
-
-  if (hasToken) {
-    if (to.path === '/login') {
-      // if is logged in, redirect to the home page
-      next({ path: '/' })
-      NProgress.done()
-    } else {
-      const hasGetUserInfo = store.getters.name
-      if (hasGetUserInfo) {
-        next()
-      } else {
-        try {
-          // get user info
-          await store.dispatch('user/getInfo')
-
-          next()
-        } catch (error) {
-          // remove token and go to login page to re-login
-          await store.dispatch('user/resetToken')
-          Message.error(error || 'Has Error')
-          next(`/login?redirect=${to.path}`)
-          NProgress.done()
-        }
-      }
-    }
-  } else {
-    /* has no token*/
-
-    if (whiteList.indexOf(to.path) !== -1) {
-      // in the free login whitelist, go directly
+//权限拦截在路由跳转  全局导航守卫 路由独享守卫 组件内守卫
+import router from '@/router' //引入路由实例
+import store from '@/store' //引入store实例 也就是this.$store
+import nProgress from 'nprogress' //引入进度条
+import 'nprogress/nprogress.css' //引入进度条样式
+//前置守卫 next必须执行 不执行的话页面无法跳转 next(false)跳转终止 
+const whiteList = ['/login','/404'] //不受权限控制的页面
+router.beforeEach((to,from,next)=>{
+  nProgress.start() //开启进度条
+  if(store.getters.token){
+    //如果有token 要去往登陆界面
+    if(to.path === '/login'){
+      next('/') //跳到主页
+    }else{
       next()
-    } else {
-      // other pages that do not have permission to access are redirected to the login page.
-      next(`/login?redirect=${to.path}`)
-      NProgress.done()
+    }
+  }else{//没有token
+    //是否在白名单
+    if(whiteList.indexOf(to.path) !== -1){
+      // 表示要去的地址在白名单
+      next()
+    }else{
+      next('/login') 
     }
   }
+  nProgress.done() //解决手动切换地址进度条不关闭
 })
-
-router.afterEach(() => {
-  // finish progress bar
-  NProgress.done()
-})
+//后置守卫
+router.afterEach((to, from) => {
+  nProgress.done() //关闭进度条
+});
+// 不需要导出 在main.js已经导入
--- src/utils/request.js
+++ src/utils/request.js
@@ -1,5 +1,6 @@
 // 引入axios
 import axios from 'axios'
+import { Message } from 'element-ui'
 //创建实例
 const service = axios.create({
   // 如果执行 npm run dev  值为 /api 正确  /api 这个代理只是给开发环境配置的代理
@@ -17,11 +18,11 @@ service.interceptors.response.use(response=>{
     if(success){
         return data
     }else{
-        // Message.error(message)//提示错误消息
+        Message.error(message)//提示错误消息
         return Promise.reject(new Error(message))
     }
 },error=>{
-    // Message.error(error.message) // 提示错误信息
+    Message.error(error.message) // 提示错误信息
      return Promise.reject(error) // 返回执行错误 让当前的执行链跳出成功 直接进入 catch
 }
  
