5a1c9cc509e453358da4b8f7b47c25259d4aa653
chuyu <1575057401@qq.com>
2022-10-20 17:44:57 +0800

    封装用户资料的action并共享用户状态

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,7 @@
 const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
-  token:state => state.user.token
+  token:state => state.user.token,
+  name:state =>state.user.userInfo.username,//建立对于用户名称的快捷访问
 }
 export default getters
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,11 +1,11 @@
  //引入token工具函数
  import {getToken,setToken,removeToken} from '@/utils/auth'
- import {login} from '@/api/user'
-import { set } from 'nprogress'
+ import {login,getUserInfo} from '@/api/user'
  //状态
  const state = {
   //设置token的初始状态
-  token:getToken()
+  token:getToken(),
+  userInfo:{} //定义一个空对象
 }
 //修改数据
 const mutations={
@@ -18,6 +18,15 @@ const mutations={
   removeToken(state){
     state.token = null //删除vuex的token
     removeToken() //删除缓存的token
+  },
+  //存储用户信息
+  saveUserInfo(state,userInfo){
+    state.userInfo = userInfo
+  },
+  //删除用户信息
+  removeUserInfo(state){
+    state.userInfo = {},
+    removeToken()
   }
 }
 //异步请求
@@ -28,7 +37,16 @@ const actions = {
     // console.log(result);
     // 修改state
     context.commit('setToken',result)
-  }
+  },
+  //获取用户信息
+  async getUserInfo(context){
+    //调用获取用户接口
+    const result =  await getUserInfo()
+    context.commit('saveUserInfo',result)
+    return result //为什么要return 后期做权限埋下伏笔
+  },
+  
+
 }
 export default {
   namespace:true,
