0fed65df6baa3f825499f22e77ffc327f49dfdd1
chuyu <1575057401@qq.com>
2022-10-22 13:56:56 +0800

    完成组织架构头部布局

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,9 +1,32 @@
 <template>
    <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        员工
-      </h2>
+        <!-- 组织架构内容 -->
+        <el-card class="tree-card">
+          <!-- 放置结构内容 -->
+          <el-row type="flex"  justify="space-between">
+            <el-col>
+              <span>江苏传智播客教育科技股份有限公司</span>
+            </el-col>
+            <el-col>
+              <el-row type="flex" justify="end">
+                <el-col>负责人</el-col>
+                <el-col :span="4">
+                  <!-- 下拉菜单 -->
+                  <el-dropdown>
+                    <!-- 内容 -->
+                    <span>操作 <i  class="el-icon-arrow-down"></i></span>
+                    <!-- 具名插槽 -->
+                    <el-dropdown-menu slot="dropdown">
+                      <!-- 下拉选项 -->
+                      <el-dropdown-item>添加子部门</el-dropdown-item>
+                    </el-dropdown-menu>
+                  </el-dropdown>
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </el-card>
     </div>
   </div>
 </template>
@@ -14,6 +37,20 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .tree-card{
+    padding: 30px 140px;
+    font-size: 14px;
+  }
+  
+</style>
 
+<style>
+  .el-dropdown-link {
+    cursor: pointer;
+    color: #409EFF;
+  }
+  .el-icon-arrow-down {
+    font-size: 12px;
+  }
 </style>
