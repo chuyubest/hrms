f01a26b40dd6db951cb5675907d367ff5722f31b
chuyu <1575057401@qq.com>
2022-10-26 19:50:25 +0800

    员工导入组件封装

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,9 @@
 //负责全局所有自定义组件的注册
 import PageTools from './PageTools'
+import UploadExcel from './UploadExcel'
 export default {
     install(Vue){
         Vue.component('PageTools',PageTools)
+        Vue.component('UploadExcel',UploadExcel)
     }
 }
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,10 +1,7 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
-    <PageTools :showBefore="true">
-        <span slot="before">呼呼</span>
-        <el-button slot="after" type="primary">导入excel</el-button>
-    </PageTools>
+    <UploadExcel/>
   </div>
 </template>
 
