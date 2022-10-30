e226dd576055f4cfbdaedf741cd1c1fe0a2c1d07
chuyu <1575057401@qq.com>
2022-10-30 15:32:03 +0800

    获取权限数据并转化为树形

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -6,13 +6,15 @@
         <el-button type="primary" slot="after" size="small">添加权限</el-button>
       </PageTools>
       <!-- 表格 -->
-      <el-table border>
-        <el-table-column  label="名称"></el-table-column>
-        <el-table-column  label="标识"></el-table-column>
-        <el-table-column  label="描述"></el-table-column>
+      <!-- 指定id为行的唯一标识 -->
+      <el-table border :data="list" row-key="id">
+        <el-table-column  label="名称" prop="name"></el-table-column>
+        <el-table-column  label="标识" prop="code"></el-table-column>
+        <el-table-column  label="描述" prop="description"></el-table-column>
         <el-table-column  label="操作">
-          <template>
-            <el-button type="text">添加</el-button>
+          <template slot-scope="{row}">
+            <!-- 添加按钮只在访问权的层级显示 当type为1才显示添加按钮 -->
+            <el-button type="text" v-if="row.type === 1" >添加</el-button>
             <el-button type="text">编辑</el-button>
             <el-button type="text">删除</el-button>
           </template>
@@ -23,8 +25,23 @@
 </template>
 
 <script>
+import {getPermissionList} from '@/api/permission'
+import {transListToTreeData} from '@/utils/index'
 export default {
-
+  data(){
+    return {
+      list:[],//所有权限
+    }
+  },
+  created(){
+    this.getPermissionList()
+  },
+  methods:{
+    async getPermissionList(){
+      //将数据转化成了树形数据
+      this.list = transListToTreeData(await getPermissionList(),'0')  
+    }
+  }
 }
 </script>
 
